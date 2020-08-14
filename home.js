8eca1cfb41780d8e674150c629be67f4fb4407dd
Dibyanshu Das <Dibyanshu2242@gmail.com>
2020-08-14 12:47:57 +0530

    New Update

--- home.js
+++ home.js
@@ -15,7 +15,33 @@ document.getElementById('someText').innerHTML = 'Hey There'; //Change the id val
 
 var age = prompt('What\'s your age');//Prompt is used for asking the user
 
-document.getElementById('age').innerHTML = 'Your age is '+age; //A simple program which can tell your age.
+document.getElementById('age').innerHTML = 'Your age is '+age; 
+//A simple program to show the user User input like in this they will write their age ,their age will be shown in the webpage.
+
+
+//Numbers in JavaScript 
+var num1 = 45;//numbers stored in variables
+
+
+//How to increase or decrease a number 
+num1 = num1 +10;     //Increase by 10
+console.log(num1)
+num1 = num1 -10;     //Decrease by 10
+console.log(num1)
+
+
+//Increment and decrement by 1 in JS
+num1++;   //Increment (Increase) of numbers
+console.log(num1)
+num1--;   //Decrement (decrease) of numbers
+console.log(num1)
+
+//All Operation on Numbers in JS
+console.log(5 * 10); //You can multiply numbers in JS
+console.log(5 / 10); //You can divide numbers in JS
+console.log(5 + 10); //You can add numbers in JS
+console.log(5 - 10); //You can subtract numbers in JS
+console.log(26 % 5); //It will show the remainder when dividing the number like 26/5 = 5 quotient and 1 remainder 
 
 
 
