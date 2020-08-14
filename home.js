d8af972c7877ce650a97467251090b480f0b3c28
Dibyanshu Das <Dibyanshu2242@gmail.com>
2020-08-14 12:21:50 +0530

    New Update

---
+++ home.js
@@ -0,0 +1,18 @@
+console.log('hello'); // it creates a statement in the console tab
+/*alert('Hey there'); //it creates a alert box*/
+
+//How to write a comment inline
+
+
+//Variables
+var b = 'smoothie'; //a string
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+document.getElementById('someText').innerHTML = 'Hey There'; //Change the id value
+
+var age = prompt('What\'s your age');//Prompt is used for asking the user
+
+document.getElementById('age').innerHTML = 'Your age is '+age; //A simple program which can tell your age.
