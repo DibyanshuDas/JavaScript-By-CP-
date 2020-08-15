56c68b02e5dccc57f7966897c597f039f3072261
Dibyanshu Das <Dibyanshu2242@gmail.com>
2020-08-15 08:13:19 +0530

    New Update

--- home.js
+++ home.js
@@ -3,7 +3,6 @@ console.log('hello'); // it creates a statement in the console tab
 
 //How to write a comment inline
 
-
 //Variables
 var b = 'smoothie'; //a string
 console.log(b);
@@ -13,41 +12,35 @@ console.log(someNumber);
 
 document.getElementById('someText').innerHTML = 'Hey There'; //Change the id value
 
-var age = prompt('What\'s your age');//Prompt is used for asking the user
+var age = prompt("What's your age"); //Prompt is used for asking the user
 
-document.getElementById('age').innerHTML = 'Your age is '+age; 
+document.getElementById('age').innerHTML = 'Your age is ' + age;
 //A simple program to show the user User input like in this they will write their age ,their age will be shown in the webpage.
 
+//Numbers in JavaScript
+var num1 = 45; //numbers stored in variables
 
-//Numbers in JavaScript 
-var num1 = 45;//numbers stored in variables
-
-
-//How to increase or decrease a number 
-num1 = num1 + 11;     //Increase by 11
-console.log(num1)
-num1 = num1 - 11;     //Decrease by 11
-console.log(num1)
-
+//How to increase or decrease a number
+num1 = num1 + 11; //Increase by 11
+console.log(num1);
+num1 = num1 - 11; //Decrease by 11
+console.log(num1);
 
 //Increment and decrement by 1 in JS
-num1++;   //Increment (Increase) of numbers
-console.log(num1)
-num1--;   //Decrement (decrease) of numbers
-console.log(num1)
+num1++; //Increment (Increase) of numbers
+console.log(num1);
+num1--; //Decrement (decrease) of numbers
+console.log(num1);
 
 //All Operation on Numbers in JS
 console.log(5 * 10); //You can multiply numbers in JS
 console.log(5 / 10); //You can divide numbers in JS
 console.log(5 + 10); //You can add numbers in JS
 console.log(5 - 10); //You can subtract numbers in JS
-console.log(26 % 5); //It will show the remainder when dividing the number like 26/5 = 5 quotient and 1 remainder 
+console.log(26 % 5); //It will show the remainder when dividing the number like 26/5 = 5 quotient and 1 remainder
 
 //Increment and decrement by any number in JS
-num1 += 10;//Increase by 10
-console.log(num1)
-num1 -= 10;//Decrease by 10
-console.log(num1)
-
-
-
+num1 += 10; //Increase by 10
+console.log(num1);
+num1 -= 10; //Decrease by 10
+console.log(num1);
